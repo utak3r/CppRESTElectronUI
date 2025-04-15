c41c723003493584bfb65afc83ac969766b910fc
utak3r <piotr.borys@gmail.com>
2025-04-15 16:44:07 +0200

    Removed unused stuff

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import SysInfo from "./sysInfo.tsx";
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
         <div>
